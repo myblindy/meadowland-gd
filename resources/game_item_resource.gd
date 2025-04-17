7afd53e49306bc158ba4e3714ca5dab5a6a96b83
Marius <tweety.04.01@gmail.com>
2025-04-16 20:59:46 -0400

    first commit

---
+++ resources/game_item_resource.gd
@@ -0,0 +1,6 @@
+class_name GameItemResource extends Resource
+
+@export var name: String
+@export var icon_texture: Texture2D
+@export var weight: float
+@export var pickup_speed_multiplier: float = 1
