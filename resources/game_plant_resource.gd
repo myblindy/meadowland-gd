8343a5bd08d1c75ee57fcf9774591c9414f76778
Marius Mitea <marius.mitea@cae.com>
2025-04-23 15:12:43 -0400

    trees!

---
+++ resources/game_plant_resource.gd
@@ -0,0 +1,7 @@
+class_name GamePlantResource extends Resource
+
+@export var name: String
+@export var sprite_texture: Texture2D
+@export var containing_resources: Array[GameResourceQuantityResource]
+@export var harvest_work_ticks: int
+@export var days_from_spawn_to_full_growth: int
