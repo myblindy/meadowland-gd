14ca07f91d6494bd213e1f9529ced7773aa0afdf
Marius Mitea <marius.mitea@cae.com>
2025-05-14 14:26:37 -0400

    finally working!

--- controls/command_bar_manager.gd
+++ controls/command_bar_manager.gd
@@ -30,20 +30,6 @@ func add_new_bar(bar: CommandBar) -> void:
 	_bars.append(bar)
 	_hook(bar)
 
-signal _await_all_done
-func _await_all(signals: Array[Signal]):
-	if signals.size():
-		var done: Array[bool]
-		done.resize(signals.size())
-
-		for _signal in signals:
-			_signal.connect(func(): 
-				done[signals.find(_signal)] = true
-				if done.all(func(b: bool) -> bool: return b):
-					_await_all_done.emit())
-
-		await _await_all_done
-
 func _hook(bar: CommandBar) -> void:
 	for button in bar.get_children():
 		if button is CommandBarButton:
@@ -57,25 +43,43 @@ func _get_canvas_relative_position(ctl: Control) -> Vector2:
 	return pos
 
 func _show_separator_left() -> void:
+	_separator_image.modulate = Color(1, 1, 1, 1)
 	_separator_image.texture = _separator_arrow_image
 	_separator_image.flip_h = true
 	_separator_image.show()
 
 func _show_separator_right() -> void:
+	_separator_image.modulate = Color(1, 1, 1, 1)
 	_separator_image.texture = _separator_arrow_image
 	_separator_image.flip_h = false
 	_separator_image.show()
 
 func _show_separator_bar() -> void:
+	_separator_image.modulate = Color(1, 1, 1, 1)
 	_separator_image.texture = _separator_bar_image
 	_separator_image.flip_h = false
 	_separator_image.show()
 	
 func _hide_separator() -> void:
 	_separator_image.hide()
+
+func _transparent_separator() -> void:
+	_separator_image.modulate = Color(1, 1, 1, 0)
+	_separator_image.show()
 	
 func _auto_separator():
-	if _current_bar_container.get_child_count() == 0 or _selected_buttons_container.get_child_count() == 0:
+	# layout isn't done until next frame
+	await RenderingServer.frame_pre_draw
+	
+	var mouse_position := get_global_mouse_position()
+	var selected_buttons_container_rect: Rect2 = _selected_buttons_container.get_global_rect()
+	var current_bar_container_rect: Rect2 = _current_bar_container.get_global_rect()
+
+	if selected_buttons_container_rect.has_point(mouse_position) and _current_bar_container.get_child_count() > 0:
+		_show_separator_right()
+	elif current_bar_container_rect.has_point(mouse_position) and _selected_buttons_container.get_child_count() > 0:
+		_show_separator_left()
+	elif _current_bar_container.get_child_count() == 0 or _selected_buttons_container.get_child_count() == 0:
 		_hide_separator()
 	else:
 		_show_separator_bar()
@@ -86,7 +90,10 @@ func _button_pressed(bar: CommandBar, button: CommandBarButton) -> void:
 	var cloned_clicked_button: CommandBarButton
 	
 	var left_slide_destination := _get_canvas_relative_position(_selected_buttons_container) \
-		+ Vector2(_selected_buttons_container.size.x + 5, 0)
+		+ Vector2(_selected_buttons_container.size.x, 0)
+
+	if _selected_buttons_container.get_child_count():
+		left_slide_destination += Vector2(5, 0)
 
 	for bar_button in bar.get_children():
 		if bar_button is CommandBarButton:
@@ -150,72 +157,79 @@ func _navigate_back(cloned_button: CommandBarButton, original_bar: CommandBar) -
 
 	# replace them with a spacer control to keep the layout the same
 	var spacer := Control.new()
-	spacer.custom_minimum_size = Vector2(0, undo_navigation_buttons.reduce(func(accum: float, ctl: Control): return accum + ctl.size.y, 0))
+	spacer.name = "spacer"
+	spacer.custom_minimum_size = Vector2(
+		undo_navigation_buttons[-1].position.x + undo_navigation_buttons[-1].size.x - undo_navigation_buttons[0].position.x,
+		0)
 
 	for navigation_button in undo_navigation_buttons:
 		_selected_buttons_container.remove_child(navigation_button)
 	_selected_buttons_container.add_child(spacer)
 
 	# slide the navigation buttons up and make them transparent
-	var tween := create_tween().set_trans(Tween.TRANS_EXPO)
+	var tween = create_tween().set_trans(Tween.TRANS_EXPO)
 	for navigation_button_idx in range(undo_navigation_buttons.size()):
 		var navigation_button := undo_navigation_buttons[navigation_button_idx]
 		get_parent().get_parent().add_child(navigation_button)
 		navigation_button.position = undo_navigation_buttons_dissolve_source[navigation_button_idx]
 		tween.parallel().tween_property(navigation_button, "position", undo_navigation_buttons_dissolve_destination[navigation_button_idx], 0.3)
 		tween.parallel().tween_property(navigation_button, "modulate", Color(1, 1, 1, 0), 0.3)
+	
+	# fade out the current bar, if any
+	for bar in _current_bar_container.get_children():
+		if bar is CommandBar:
+			for button in bar.get_children():
+				if button is CommandBarButton:
+					cloned_button = button.duplicate()
+					cloned_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
+					var original_position := _get_canvas_relative_position(button)
+					var destination_position := original_position + Vector2(0, button.size.y * 3)
+					cloned_button.position = original_position 
+					cloned_button.modulate = Color(1, 1, 1, 1)
+					cloned_button.add_to_group(_temp_command_bar_button_group)
+					get_parent().get_parent().add_child(cloned_button)
+
+					tween.parallel().tween_property(cloned_button, "position", destination_position, 0.3)
+					tween.parallel().tween_property(cloned_button, "modulate", Color(1, 1, 1, 0), 0.3)
+
+	while _current_bar_container.get_child_count():
+		_current_bar_container.remove_child(_current_bar_container.get_child(0))
 	await tween.finished
 	
 	# spacer goes bye bye
 	spacer.get_parent().remove_child(spacer)
 	spacer.queue_free()
-	
-	# fade out the current bar, if any
-	tween = create_tween().set_trans(Tween.TRANS_EXPO)
-	if _current_bar_container.get_child_count():
-		for bar in _current_bar_container.get_children():
-			if bar is CommandBar:
-				for button in bar.get_children():
-					if button is CommandBarButton:
-						cloned_button = button.duplicate()
-						cloned_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-						var destination_position := _get_canvas_relative_position(button)
-						cloned_button.position = destination_position + Vector2(0, button.size.y * 3)
-						cloned_button.modulate = Color(1, 1, 1, 0)
-						cloned_button.add_to_group(_temp_command_bar_button_group)
-						get_parent().get_parent().add_child(cloned_button)
-
-						tween.parallel().tween_property(cloned_button, "position", destination_position, 0.3)
-						tween.parallel().tween_property(cloned_button, "modulate", Color(1, 1, 1, 1), 0.3)
 
 	# fade in the original bar
-	while _current_bar_container.get_child_count():
-		_current_bar_container.remove_child(_current_bar_container.get_child(0))
-	
 	_current_bar_container.add_child(original_bar)
 	original_bar.modulate = Color(1, 1, 1, 0)
-	_show_separator_bar()
+	if _selected_buttons_container.get_child_count():
+		_transparent_separator()
+	else:
+		_hide_separator()
 	await RenderingServer.frame_pre_draw
 	
+	tween = create_tween().set_trans(Tween.TRANS_EXPO)
 	for original_bar_button in original_bar.get_children():
 		if original_bar_button is CommandBarButton:
 			var cloned_original_button := original_bar_button.duplicate()
 			cloned_original_button.mouse_filter = Control.MOUSE_FILTER_IGNORE
-			var destination_position := _get_canvas_relative_position(original_bar_button)
-			cloned_original_button.position = destination_position - Vector2(0, original_bar_button.size.y * 3)
+			var original_position := _get_canvas_relative_position(original_bar_button)
+			cloned_original_button.position = original_position - Vector2(0, original_bar_button.size.y * 3)
 			cloned_original_button.modulate = Color(1, 1, 1, 0)
 			cloned_original_button.add_to_group(_temp_command_bar_button_group)
 			get_parent().get_parent().add_child(cloned_original_button)
 			
-			tween.parallel().tween_property(cloned_original_button, "position", destination_position, 0.3)
+			tween.parallel().tween_property(cloned_original_button, "position", original_position, 0.3)
 			tween.parallel().tween_property(cloned_original_button, "modulate", Color(1, 1, 1, 1), 0.3)
+			
 	await tween.finished
 	
 	# fading is done, get rid of the cloned buttons and show the real bar
 	get_tree().call_group(_temp_command_bar_button_group, "queue_free")
 	original_bar.modulate = Color(1, 1, 1, 1)
 
-	_auto_separator()
+	await _auto_separator()
 
 func _on_selected_buttons_container_mouse_entered() -> void:
 	if _selected_buttons_container.get_child_count():
@@ -226,7 +240,7 @@ func _on_current_bar_container_mouse_entered() -> void:
 		_show_separator_left()
 
 func _on_current_bar_container_mouse_exited() -> void:
-	_auto_separator()
+	await _auto_separator()
 
 func _on_selected_buttons_container_mouse_exited() -> void:
-	_auto_separator()
+	await _auto_separator()
